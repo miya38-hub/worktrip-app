e62eefae479faa93312dab9a414d34c817e25f4f
miya38-hub <miyacharge3803@gmail.com>
2026-04-26 08:06:46 +0000

    fix: active_storage host設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  Rails.application.routes.default_url_options[:host] = "worktrip-app.com"
+  Rails.application.routes.default_url_options = {
+    host: "worktrip-app.com",
+    protocol: "https"
+  }
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
 
